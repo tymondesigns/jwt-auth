1eba82242bd37ebb24acc33be12efd6728fad0b3
Sean <tymon148@gmail.com>
2014-09-16 22:35:07 +0100

    adding JWTAuth spec

---
+++ spec/Tymon/JWTAuth/JWTAuthSpec.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace spec\Tymon\JWTAuth;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Tymon\JWTAuth\JWTProvider;
+use Mockery;
+
+class JWTAuthSpec extends ObjectBehavior
+{
+    function it_is_initializable(JWTProvider $provider)
+    {
+    	$auth = Mockery::mock('Illuminate\Auth\AuthManager');
+    	$this->beConstructedWith($provider, $auth);
+
+        $this->shouldHaveType('Tymon\JWTAuth\JWTAuth');
+    }
+}
