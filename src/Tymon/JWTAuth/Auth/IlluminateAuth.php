992460eb1a65052ecfbaaea1ecabc8aa16f459bf
Sean <tymon148@gmail.com>
2014-10-26 21:58:48 +0000

    formatting

--- src/Tymon/JWTAuth/Auth/IlluminateAuth.php
+++ src/Tymon/JWTAuth/Auth/IlluminateAuth.php
@@ -6,7 +6,7 @@ use Illuminate\Auth\AuthManager;
 class IlluminateAuth implements AuthInterface
 {
 
-	/**
+    /**
      * @var \Illuminate\Auth\AuthManager
      */
     protected $auth;
@@ -14,41 +14,41 @@ class IlluminateAuth implements AuthInterface
     /**
      * @param \Illuminate\Auth\AuthManager $auth
      */
-	public function __construct(AuthManager $auth)
-	{
-		$this->auth = $auth;
-	}
+    public function __construct(AuthManager $auth)
+    {
+        $this->auth = $auth;
+    }
 
-	/**
-	 * Log a user into the application without sessions or cookies.
-	 *
-	 * @param  array  $credentials
-	 * @return bool
-	 */
-	public function check(array $credentials = [])
-	{
-		return $this->auth->once($credentials);
-	}
+    /**
+     * Log a user into the application without sessions or cookies.
+     *
+     * @param  array  $credentials
+     * @return bool
+     */
+    public function check(array $credentials = [])
+    {
+        return $this->auth->once($credentials);
+    }
 
-	/**
-	 * Log the given user ID into the application without sessions or cookies.
-	 * 
-	 * @param  mixed  $id
-	 * @return bool
-	 */
-	public function checkUsingId($id)
-	{
-		return $this->auth->onceUsingId($id);
-	}
+    /**
+     * Log the given user ID into the application without sessions or cookies.
+     * 
+     * @param  mixed  $id
+     * @return bool
+     */
+    public function checkUsingId($id)
+    {
+        return $this->auth->onceUsingId($id);
+    }
 
-	/**
-	 * Get the currently authenticated user.
-	 *
-	 * @return mixed
-	 */
-	public function user()
-	{
-		return $this->auth->user();
-	}
+    /**
+     * Get the currently authenticated user.
+     *
+     * @return mixed
+     */
+    public function user()
+    {
+        return $this->auth->user();
+    }
 
 }
