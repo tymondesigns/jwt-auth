afef75c189c82240ba0356e8d7da502cf21097a1
Sean Tymon <tymon148@gmail.com>
2014-10-28 08:49:02 +0000

    adding functionality and implementing interface

--- src/Tymon/JWTAuth/Storage/IlluminateCacheAdapter.php
+++ src/Tymon/JWTAuth/Storage/IlluminateCacheAdapter.php
@@ -1,11 +1,21 @@
 <?php namespace Tymon\JWTAuth\Storage;
 
 use Illuminate\Cache\Repository;
+use Tymon\JWTAuth\Storage\Storable;
 
-class IlluminateCacheAdapter
+class IlluminateCacheAdapter implements Storable
 {
+    /**
+     * @var \Illuminate\Cache\Repository
+     */
+    protected $cache;
+    
+    /**
+     * @var string
+     */
+    protected $tag = 'tymon.jwt';
 
-	/**
+    /**
      * @param \Illuminate\Cache\Repository  $cache
      */
     public function __construct(Repository $cache)
@@ -13,18 +23,23 @@ class IlluminateCacheAdapter
         $this->cache = $cache;
     }
 
-	public function add($token)
+	public function add($key, $value, $minutes)
 	{
-		// $this->cache->put($token)
+		return $this->cache->tags($this->tag)->put($key, $value, $minutes);
 	}
 
-	public function exists($token)
+	public function has($key)
 	{
-
+        return $this->cache->tags($this->tag)->has($key);
 	}
 
-	public function destroy($token)
+	public function destroy($key)
 	{
-
+        return $this->cache->tags($this->tag)->forget($key);
+	}
+	
+	public function flush()
+	{
+	    return $this->cache->tags($this->tag)->flush();
 	}
 }
