7e9886992cb4649e004fd5bc59dde1b976c300fd
Sean <tymon148@gmail.com>
2014-12-22 00:31:08 +0000

    adding psecio adapter

--- src/Providers/JWT/PsecioAdapter.php
+++ src/Providers/JWT/PsecioAdapter.php
@@ -3,11 +3,11 @@
 namespace Tymon\JWTAuth\Providers\JWT;
 
 use Exception;
-use JWT as Firebase;
+use Psecio\Jwt\Jwt;
+use Psecio\Jwt\Header;
 use Tymon\JWTAuth\Exceptions\JWTException;
-use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 
-class FirebaseAdapter extends JWTProvider implements JWTInterface
+class PsecioAdapter extends JWTProvider implements JWTInterface
 {
     /**
      * Create a JSON Web Token
@@ -18,7 +18,7 @@ class FirebaseAdapter extends JWTProvider implements JWTInterface
     public function encode(array $payload)
     {
         try {
-            return Firebase::encode($payload, $this->secret, $this->algo);
+            return with(new Jwt($this->getHeader()))->encode($payload);
         } catch (Exception $e) {
             throw new JWTException('Could not create token: ' . $e->getMessage());
         }
@@ -34,14 +34,21 @@ class FirebaseAdapter extends JWTProvider implements JWTInterface
     public function decode($token)
     {
         try {
-            return (array) Firebase::decode($token, $this->secret);
+            return with(new Jwt($this->getHeader()))->decode($token);
         } catch (Exception $e) {
-            // firebase implementation doesn't return the payload if it has expired
-            if ($e->getMessage() === 'Expired Token') {
-                throw new TokenExpiredException('JWT has expired');
-            } else {
+            if (get_class($e) === 'Psecio\Jwt\Exception\BadSignatureException') {
                 throw new JWTException('Could not decode token: ' . $e->getMessage());
             }
         }
+    }
+
+    /**
+     * Get the JWT Header instance
+     *
+     * @return \Psecio\Jwt\Header
+     */
+    protected function getHeader()
+    {
+        return with(new Header($this->secret))->setAlgorithm($this->algo);
     }
 }
