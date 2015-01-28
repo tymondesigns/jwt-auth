eb1e8c7a5713824a30585f716969173a72277321
Sean Tymon <tymon148@gmail.com>
2015-01-28 09:22:47 +0000

    fix formatting

--- src/Utils.php
+++ src/Utils.php
@@ -7,24 +7,24 @@ use Carbon\Carbon;
 class Utils
 {
 
-	/**
+    /**
      * Get the Carbon instance for the current time
      *
      * @return \Carbon\Carbon
      */
-	public static function now()
-	{
-		return Carbon::now();
-	}
+    public static function now()
+    {
+        return Carbon::now();
+    }
 
-	/**
+    /**
      * Get the Carbon instance for the timestamp
      *
      * @param  int  $timestamp
      * @return \Carbon\Carbon
      */
-	public static function timestamp($timestamp)
-	{
-		return Carbon::createFromTimeStampUTC($timestamp);
-	}
+    public static function timestamp($timestamp)
+    {
+        return Carbon::createFromTimeStampUTC($timestamp);
+    }
 }
