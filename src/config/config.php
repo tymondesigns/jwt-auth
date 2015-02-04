1aba514d1eb8b50fba0ca4d355913f608f3bfa1a
Sean Tymon <tymon148@gmail.com>
2015-02-04 23:54:54 +0000

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_KEY', 'changeme'),
+    'secret' => env('JWT_SECRET', 'changeme'),
 
     /*
     |--------------------------------------------------------------------------
