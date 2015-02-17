70f4d2eec6445e07dcfa57fdbaebebd82233bb84
Sean <tymon148@gmail.com>
2015-02-17 20:36:30 +0000

    implementing PR #45

--- tests/PayloadFactoryTest.php
+++ tests/PayloadFactoryTest.php
@@ -72,6 +72,27 @@ class PayloadFactoryTest extends \PHPUnit_Framework_TestCase
     }
 
     /** @test */
+    public function it_should_return_a_payload_when_passing_miltidimensional_array_as_custom_claim_to_make_method()
+    {
+        $this->validator->shouldReceive('setRefreshFlow->check');
+
+        $this->claimFactory->shouldReceive('get')->once()->with('sub', 1)->andReturn(new Subject(1));
+        $this->claimFactory->shouldReceive('get')->once()->with('iss', Mockery::any())->andReturn(new Issuer('/foo'));
+        $this->claimFactory->shouldReceive('get')->once()->with('exp', Mockery::any())->andReturn(new Expiration(time() + 3600));
+        $this->claimFactory->shouldReceive('get')->once()->with('iat', Mockery::any())->andReturn(new IssuedAt(time()));
+        $this->claimFactory->shouldReceive('get')->once()->with('jti', Mockery::any())->andReturn(new JwtId('foo'));
+        $this->claimFactory->shouldReceive('get')->once()->with('nbf', Mockery::any())->andReturn(new NotBefore(time()));
+        $this->claimFactory->shouldReceive('get')->once()->with('foo', ['bar' => [0,0,0]])->andReturn(new Custom('foo', ['bar' => [0,0,0]]));
+
+        $payload = $this->factory->sub(1)->foo(['bar' => [0,0,0]])->make();
+
+        $this->assertEquals($payload->get('sub'), 1);
+        $this->assertEquals($payload->get('foo'), ['bar' => [0,0,0]]);
+
+        $this->assertInstanceOf('Tymon\JWTAuth\Payload', $payload);
+    }
+
+    /** @test */
     public function it_should_set_the_ttl()
     {
         $this->factory->setTTL(12345);
