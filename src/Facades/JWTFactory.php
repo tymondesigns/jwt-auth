c4689c01af1145fe4a1a48c2e187a896640a95d7
Sean <tymon148@gmail.com>
2015-04-20 20:20:44 +0100

    fixing token refreshing and adding JWTFactory facade

---
+++ src/Facades/JWTFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tymon\JWTAuth\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class JWTFactory extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'tymon.jwt.payload.factory';
+    }
+}
