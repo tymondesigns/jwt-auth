52e0d23986ac873038a86e1d53e47e238959fcec
Mark <hc.devmark@gmail.com>
2015-04-21 16:30:10 +0800

    clone new jwt provider without auth

---
+++ src/Facades/JWT.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tymon\JWTAuth\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class JWT extends Facade
+{
+  /**
+   * Get the registered name of the component.
+   *
+   * @return string
+   */
+  protected static function getFacadeAccessor()
+  {
+    return 'tymon.jwt';
+  }
+}
