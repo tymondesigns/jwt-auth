3432e5a0efa1b94915987d747272e43ff0bdfe48
Sean <tymon148@gmail.com>
2015-07-04 23:32:20 +0100

    cs-fix

--- tests/Stubs/SentinelStub.php
+++ tests/Stubs/SentinelStub.php
@@ -6,24 +6,20 @@ use Cartalyst\Sentinel\Users\UserInterface;
 
 class SentinelStub implements UserInterface
 {
-
     public function getUserId()
     {
         return 123;
     }
-
 
     public function getUserLogin()
     {
         return 'foo';
     }
 
-
     public function getUserLoginName()
     {
         return 'bar';
     }
-
 
     public function getUserPassword()
     {
