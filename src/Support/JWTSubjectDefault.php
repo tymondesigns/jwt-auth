1c5ca44e4b24c43f47b6f196010506ff6c9e5cfe
Sean <tymon148@gmail.com>
2015-08-24 19:45:05 +0100

    adding default subject trait

---
+++ src/Support/JWTSubjectDefault.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of jwt-auth
+ *
+ * (c) Sean Tymon <tymon148@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tymon\JWTAuth\Support;
+
+trait JWTSubjectDefault
+{
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
