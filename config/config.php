a572adf22703a9a838be3ab673c24c183c8bd13d
Sean <tymon148@gmail.com>
2015-10-03 14:22:24 +0100

    update config

--- config/config.php
+++ config/config.php
@@ -20,7 +20,12 @@ return [
     |--------------------------------------------------------------------------
     |
     | Specify the length of time (in minutes) that the token will be valid for.
-    | Defaults to 1 hour
+    | Defaults to 1 hour.
+    |
+    | You can also set this to null, to yield a never expiring token.
+    | Some people may want this behaviour for e.g. a mobile app.
+    | This is not particularly recommended, so make sure you have appropriate
+    | systems in place to revoke the token if necessary.
     |
     */
 
