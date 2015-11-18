3d15e5f41755b5bc35a99a88d2358de0d81722e8
Sean <tymon148@gmail.com>
2015-11-18 20:29:27 +0000

    update key config

--- config/config.php
+++ config/config.php
@@ -7,8 +7,9 @@ return [
     | JWT Authentication Secret
     |--------------------------------------------------------------------------
     |
-    | Don't forget to set this, as it will be used to sign your tokens.
-    | A helper command is provided for this: `php artisan jwt:generate`
+    | Don't forget to set this in your .env file, as it will be used to sign
+    | your tokens. A helper command is provided for this:
+    | `php artisan jwt:secret`
     |
     */
 
