e39353a8720948ebc9a593e9965e7df72b6f205c
Sean Tymon <tymon148@gmail.com>
2016-01-25 17:16:01 -0500

    Applied fixes from StyleCI

--- tests/Providers/User/EloquentUserAdapterTest.php
+++ tests/Providers/User/EloquentUserAdapterTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of jwt-auth
+ *
+ * (c) Sean Tymon <tymon148@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Tymon\JWTAuth\Test\Providers\User;
 
 use Mockery;
