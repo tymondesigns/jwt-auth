ec43a55232be5984c116dd7477825bb7c04adc6a
Sean <tymon148@gmail.com>
2016-01-28 19:53:34 +0000

    more test coverage

--- tests/JWTGuardTest.php
+++ tests/JWTGuardTest.php
@@ -30,7 +30,7 @@ class JWTGuardTest extends \PHPUnit_Framework_TestCase
     }
 
     /** @test */
-    public function it_should_get_the_authenticated_user()
+    public function it_should_get_the_authenticated_user_if_a_valid_token_is_provided()
     {
         $this->jwt->shouldReceive('getToken')->andReturn('foo.bar.baz');
         $this->jwt->shouldReceive('check')->andReturn(true);
@@ -38,6 +38,20 @@ class JWTGuardTest extends \PHPUnit_Framework_TestCase
 
         $this->provider->shouldReceive('retrieveById')->with(1)->once()->andReturn((object) ['id' => 1]);
         $this->assertSame(1, $this->guard->user()->id);
+
+        // check that the user is stored on the object
+        $this->assertSame(1, $this->guard->user()->id);
+    }
+
+    /** @test */
+    public function it_should_return_null_if_an_invalid_token_is_provided()
+    {
+        $this->jwt->shouldReceive('getToken')->andReturn('invalid.token.here');
+        $this->jwt->shouldReceive('check')->andReturn(false);
+        $this->jwt->shouldReceive('getPayload->get')->never();
+        $this->provider->shouldReceive('retrieveById')->never();
+
+        $this->assertNull($this->guard->user());
     }
 
 }
