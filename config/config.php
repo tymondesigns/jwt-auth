c330d224cc7c097e84c5ed43ef3bdef6e0558a4b
Sean Tymon <tymon148@gmail.com>
2016-01-29 17:54:28 -0500

    Applied fixes from StyleCI

--- config/config.php
+++ config/config.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of jwt-auth.
+ *
+ * @author Sean Tymon <tymon148@gmail.com>
+ * @copyright Copyright (c) Sean Tymon
+ * @link https://github.com/tymondesigns/jwt-auth
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 return [
 
     /*
@@ -142,8 +153,8 @@ return [
         |
         */
 
-        'storage' => Tymon\JWTAuth\Providers\Storage\Illuminate::class
+        'storage' => Tymon\JWTAuth\Providers\Storage\Illuminate::class,
 
-    ]
+    ],
 
 ];
