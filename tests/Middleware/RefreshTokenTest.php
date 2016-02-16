467a87fdb44c0d8f4f558173e0b95973ca26a921
Lucas Michot <lucas@semalead.com>
2016-02-16 21:44:40 +0100

    Add an empty line

--- tests/Middleware/RefreshTokenTest.php
+++ tests/Middleware/RefreshTokenTest.php
@@ -28,6 +28,7 @@ class RefreshTokenTest extends AbstractTestCase
      * @var \Mockery\MockInterface
      */
     protected $request;
+    
     /**
      * @var \Tymon\JWTAuth\Middleware\RefreshToken
      */
