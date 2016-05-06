7158d99b470c034183d47c45ca7cf0670376f056
Sean <tymon148@gmail.com>
2016-05-06 22:47:29 +0100

    adding cookie parser

---
+++ src/Http/Parser/Cookies.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of jwt-auth.
+ *
+ * (c) Sean Tymon <tymon148@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tymon\JWTAuth\Http\Parser;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Contracts\Http\Parser as ParserContract;
+
+class Cookies implements ParserContract
+{
+    /**
+     * The input source key.
+     *
+     * @var string
+     */
+    protected $key = 'token';
+
+    /**
+     * Try to parse the token from the request cookies.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return null|string
+     */
+    public function parse(Request $request)
+    {
+        return $request->cookie($this->key);
+    }
+
+    /**
+     * Set the cookie key.
+     *
+     * @param  string  $key
+     *
+     * @return $this
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+}
