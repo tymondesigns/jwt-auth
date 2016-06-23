a82685c0d2c0bccd0a00b3ebe5c2cf0890509202
Jaff Parker <andreykamozin@gmail.com>
2016-06-23 15:27:01 -0400

    Adding JWTGuard->userOrFail() method throw an exception in case of failure (#762)

---
+++ src/Exceptions/UserNotDefinedException.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of jwt-auth.
+ *
+ * (c) Sean Tymon <tymon148@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tymon\JWTAuth\Exceptions;
+
+class UserNotDefinedException extends JWTException
+{
+    //
+}
