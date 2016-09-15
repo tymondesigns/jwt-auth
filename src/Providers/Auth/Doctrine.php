612c8ff3c492626682e5ed25fcd85cdbd15b47b0
jkirkby91-2 <me@jameskirkby.com>
2016-09-15 15:07:38 +0100

    Update Doctrine.php
    fix styleCI issues

--- src/Providers/Auth/Doctrine.php
+++ src/Providers/Auth/Doctrine.php
@@ -1,12 +1,21 @@
 <?php
 
+/*
+* This file is part of jwt-auth.
+*
+* (c) Sean Tymon <tymon148@gmail.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace Tymon\JWTAuth\Providers\Auth;
 
 use Tymon\JWTAuth\Contracts\Providers\Auth;
 use LaravelDoctrine\ORM\Auth\DoctrineUserProvider;
 
 /**
- * Class Doctrine
+ * Class Doctrine.
  *
  * @package Tymon\JWTAuth\Providers\Auth
  * @author James Kirkby <me@jameskirkby.com>
@@ -46,11 +55,13 @@ class Doctrine implements Auth
 
     /**
      * @param mixed $id
+     * 
      * @return mixed
      */
     public function byId($id)
     {
         $this->user = $this->doctrineUserAdapter->retrieveById($id);
+        
         return $this->user;
     }
 
@@ -61,5 +72,4 @@ class Doctrine implements Auth
     {
         return $this->user;
     }
-
 }
