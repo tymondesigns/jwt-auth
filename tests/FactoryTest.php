615d9386ecde95f7b390088434033b5d10e01ee5
William Seward <wrseward@users.noreply.github.com>
2016-11-18 15:27:11 -0500

    Reset factory claims after making a payload

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -191,6 +191,34 @@ class FactoryTest extends AbstractTestCase
     }
 
     /** @test */
+    public function it_should_exclude_claims_from_previous_payloads()
+    {
+        $fooClaim = new Custom('foo', 'bar');
+        $barClaim = new Custom('baz', 'qux');
+
+        $this->claimFactory->shouldReceive('getTTL')->andReturn(60);
+        $this->claimFactory->shouldReceive('get')->with('foo', 'bar')->twice()->andReturn($fooClaim);
+        $this->claimFactory->shouldReceive('get')->with('baz', 'qux')->once()->andReturn($barClaim);
+        $this->validator->shouldReceive('setRefreshFlow->check')->once()->andReturn(new Collection([$fooClaim, $barClaim]));
+
+        $payload = $this->factory->setDefaultClaims([])
+            ->customClaims([
+                'foo' => 'bar',
+                'baz' => 'qux',
+            ])->make();
+
+        $this->assertSame($payload->get('foo'), 'bar');
+        $this->assertSame($payload->get('baz'), 'qux');
+
+        $this->validator->shouldReceive('setRefreshFlow->check')->once()->andReturn(new Collection([$fooClaim]));
+
+        $payload = $this->factory->setDefaultClaims([])->customClaims(['foo' => 'bar'])->make();
+
+        $this->assertSame($payload->get('foo'), 'bar');
+        $this->assertFalse($payload->hasKey('baz'));
+    }
+
+    /** @test */
     public function it_should_set_the_default_claims()
     {
         $this->factory->setDefaultClaims(['sub', 'iat']);
