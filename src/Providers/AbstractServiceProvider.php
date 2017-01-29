3f0b23118dae645c8e0b9d2ffa42390a1a0253c7
Sean Tymon <tymon148@gmail.com>
2017-01-29 21:40:40 +0000

    adding cookie parser to default parser chain

--- src/Providers/AbstractServiceProvider.php
+++ src/Providers/AbstractServiceProvider.php
@@ -18,6 +18,7 @@ use Tymon\JWTAuth\Manager;
 use Tymon\JWTAuth\JWTGuard;
 use Tymon\JWTAuth\Blacklist;
 use Tymon\JWTAuth\Http\Parser\Parser;
+use Tymon\JWTAuth\Http\Parser\Cookies;
 use Illuminate\Support\ServiceProvider;
 use Tymon\JWTAuth\Http\Parser\AuthHeaders;
 use Tymon\JWTAuth\Http\Parser\InputSource;
@@ -175,7 +176,13 @@ abstract class AbstractServiceProvider extends ServiceProvider
         $this->app->singleton('tymon.jwt.parser', function ($app) {
             $parser = new Parser(
                 $app['request'],
-                [new AuthHeaders, new QueryString, new InputSource, new RouteParams]
+                [
+                    new AuthHeaders,
+                    new QueryString,
+                    new InputSource,
+                    new RouteParams,
+                    new Cookies,
+                ]
             );
 
             $app->refresh('request', $parser, 'setRequest');
@@ -311,7 +318,7 @@ abstract class AbstractServiceProvider extends ServiceProvider
         $instance = $this->config($key);
 
         if (is_string($instance)) {
-            return $this->app->make($instance);
+            return new $instance;
         }
 
         return $instance;
--- src/Providers/LaravelServiceProvider.php
+++ src/Providers/LaravelServiceProvider.php
@@ -28,18 +28,31 @@ class LaravelServiceProvider extends AbstractServiceProvider
         $this->publishes([$path => config_path('jwt.php')], 'config');
         $this->mergeConfigFrom($path, 'jwt');
 
-        if (method_exists($this->app['router'], 'aliasMiddleware')) {
-            $this->app['router']->aliasMiddleware('jwt.auth', Authenticate::class);
-            $this->app['router']->aliasMiddleware('jwt.refresh', RefreshToken::class);
-            $this->app['router']->aliasMiddleware('jwt.renew', AuthenticateAndRenew::class);
-            $this->app['router']->aliasMiddleware('jwt.check', Check::class);
-        } else {
-            $this->app['router']->middleware('jwt.auth', Authenticate::class);
-            $this->app['router']->middleware('jwt.refresh', RefreshToken::class);
-            $this->app['router']->middleware('jwt.renew', AuthenticateAndRenew::class);
-            $this->app['router']->middleware('jwt.check', Check::class);
-        }
+        $this->aliasMiddleware('jwt.auth', Authenticate::class);
+        $this->aliasMiddleware('jwt.refresh', RefreshToken::class);
+        $this->aliasMiddleware('jwt.renew', AuthenticateAndRenew::class);
+        $this->aliasMiddleware('jwt.check', Check::class);
 
         $this->extendAuthGuard();
+    }
+
+    /**
+     * Register a middleware alias.
+     *
+     * @param  string $name
+     * @param  string $class
+     *
+     * @return \Illuminate\Routing\Router
+     */
+    protected function aliasMiddleware($name, $class)
+    {
+        $router = $this->app['router'];
+
+        // the method name was changed in Laravel 5.4
+        if (method_exists($router, 'aliasMiddleware')) {
+            return $router->aliasMiddleware($name, $class);
+        }
+
+        return $router->middleware($name, $class);
     }
 }
