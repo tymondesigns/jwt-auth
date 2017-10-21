4e6ca2def709b9e416b3523a9629ffc4d7640feb
Muhammad Akbar <bonntt@gmail.com>
2017-10-22 00:53:37 +0500

    Exceptions caught and raised accordingly
    The exceptions can be handled in exception handler class whenever the middleware is brought to use before 'auth' middleware. Useful in Lumen 5.5 as the normal response from auth middleware is a HTTP 401 without any description.

--- src/Http/Middleware/Check.php
+++ src/Http/Middleware/Check.php
@@ -43,8 +43,9 @@ class Check extends BaseMiddleware
                     throw new TokenBlacklistedException();
                 } elseif ($exception instanceof PayloadException) {
                     throw new PayloadException();
-                } 
-                throw new JWTException('unknown issue');
+                } else {
+                    throw new JWTException('unknown issue');
+                }
             }
         } else {
             throw new JWTException('no token');
