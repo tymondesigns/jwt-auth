c23b48f476f77659657dc8b340ed6e9541400d27
Junior Grossi <juniorgro@gmail.com>
2018-10-26 22:43:38 -0200

    Overrides the actingAs() method using a trait

---
+++ src/Claims/JwtAuthentication.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of jwt-auth.
+ *
+ * (c) Sean Tymon <tymon148@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tymon\JWTAuth\Claims;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+trait JwtAuthentication
+{
+    /**
+     * Overrides the default actingAs() method to set the user token when
+     * testing in Laravel.
+     *
+     * @param Authenticatable $user
+     * @param null|string $driver
+     * @return $this
+     */
+    public function actingAs(Authenticatable $user, $driver = null)
+    {
+        if (method_exists($this, 'withHeader')) {
+            $token = JWTAuth::fromUser($user);
+            $this->withHeader('Authorization', 'Bearer ' . $token);
+        }
+
+        return $this;
+    }
+}
