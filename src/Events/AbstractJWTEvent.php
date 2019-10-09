75c4f6aa83ecdfe4bcfe4c1ee116a1ca3475cf01
Gabriel Gonçalves <contato@gabrieldeveloper.com.br>
2019-10-09 03:20:14 -0300

    Add Events

---
+++ src/Events/AbstractJWTEvent.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * This file is part of jwt-auth.
+ *
+ * (c) Sean Tymon <tymon148@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tymon\JWTAuth\Events;
+abstract class AbstractJWTEvent
+{
+    /**
+     * The authenticated user.
+     *
+     * @var \Illuminate\Contracts\Auth\Authenticatable
+     */
+    public $user;
+    /**
+     * @var \Tymon\JWTAuth\Token
+     */
+    public $token;
+    /**
+     * Create a new event instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  \Tymon\JWTAuth\Token|string  $token
+     *
+     * @return void
+     */
+    public function __construct($user, $token)
+    {
+        $this->token = $token;
+        $this->user = $user;
+    }
+}
---
+++ src/Events/JWTAttempt.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of jwt-auth.
+ *
+ * (c) Sean Tymon <tymon148@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tymon\JWTAuth\Events;
+
+class JWTAttempt extends AbstractJWTEvent
+{
+    public function __construct($user)
+    {
+        parent::__construct($user, null);
+    }
+}
