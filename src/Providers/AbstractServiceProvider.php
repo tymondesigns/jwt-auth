0d4c023ed6873cd5b5121886523fe0941a266fa9
Gabriel Gonçalves <contato@gabrieldeveloper.com.br>
2019-10-09 03:21:52 -0300

    Change Service Provider

--- src/Providers/AbstractServiceProvider.php
+++ src/Providers/AbstractServiceProvider.php
@@ -99,7 +99,8 @@ abstract class AbstractServiceProvider extends ServiceProvider
             $guard = new JWTGuard(
                 $app['tymon.jwt'],
                 $app['auth']->createUserProvider($config['provider']),
-                $app['request']
+                $app['request'],
+                $app['events']
             );
 
             $app->refresh('request', $guard, 'setRequest');
