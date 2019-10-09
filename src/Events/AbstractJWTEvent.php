fb7b863e53f52f0504d93b947d13b1765cb079e5
Gabriel Gonçalves <contato@gabrieldeveloper.com.br>
2019-10-09 03:34:09 -0300

    Fix StyleCi issues

--- src/Events/AbstractJWTEvent.php
+++ src/Events/AbstractJWTEvent.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This file is part of jwt-auth.
  *
@@ -7,7 +8,9 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Tymon\JWTAuth\Events;
+
 abstract class AbstractJWTEvent
 {
     /**
@@ -20,6 +23,7 @@ abstract class AbstractJWTEvent
      * @var \Tymon\JWTAuth\Token
      */
     public $token;
+
     /**
      * Create a new event instance.
      *
