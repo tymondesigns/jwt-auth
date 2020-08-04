1678d096ec6777f7171a0be4de216cbbd6e14fbd
wushunyi <wushunyi@live.com>
2020-08-04 08:47:18 +0800

    add storage cahce config

---
+++ src/Providers/LaravelServiceProvider.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of jwt-auth.
+ *
+ * (c) Sean Tymon <tymon148@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tymon\JWTAuth\Providers;
+
+class LaravelServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        $path = realpath(__DIR__.'/../../config/config.php');
+
+        $this->publishes([$path => config_path('jwt.php')], 'config');
+        $this->mergeConfigFrom($path, 'jwt');
+
+        $this->aliasMiddleware();
+
+        $this->extendAuthGuard();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerStorageProvider()
+    {
+        $this->app->singleton('tymon.jwt.provider.storage', function () {
+            $instance = $this->getConfigInstance('providers.storage');
+
+            if (method_exists($instance, 'setLaravelVersion')) {
+                $instance->setLaravelVersion($this->app->version());
+            }
+
+            return $instance;
+        });
+
+        if ($store = $this->config('cache')) {
+            $this->app
+                ->when(\Tymon\JWTAuth\Providers\Storage\Illuminate::class)
+                ->needs(\Illuminate\Contracts\Cache\Repository::class)
+                ->give(
+                    function () use ($store) {
+                        return cache()->store($store);
+                    }
+                );
+        }
+    }
+
+    /**
+     * Alias the middleware.
+     *
+     * @return void
+     */
+    protected function aliasMiddleware()
+    {
+        $router = $this->app['router'];
+
+        $method = method_exists($router, 'aliasMiddleware') ? 'aliasMiddleware' : 'middleware';
+
+        foreach ($this->middlewareAliases as $alias => $middleware) {
+            $router->$method($alias, $middleware);
+        }
+    }
+}
