a9e057080c64bb61ca6b89c82932811fc8b0c171
Sean Tymon <tymon148@gmail.com>
2023-02-14 11:14:55 +0000

    chore(deps): Add Laravel 10 support

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -11,9 +11,9 @@
 
 namespace Tymon\JWTAuth\Test;
 
-use Carbon\Carbon;
 use Mockery;
-use Yoast\PHPUnitPolyfills\TestCases\TestCase;
+use Carbon\Carbon;
+use PHPUnit\Framework\TestCase;
 
 abstract class AbstractTestCase extends TestCase
 {
