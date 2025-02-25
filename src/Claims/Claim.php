4ab5db6581624d6bb42b7b6d473e2777773accb1
Sean Tymon <tymon148@gmail.com>
2025-02-25 10:49:11 +0000

    chore: Allow Laravel 12 and other improvements

--- src/Claims/Claim.php
+++ src/Claims/Claim.php
@@ -139,7 +139,7 @@ abstract class Claim implements Arrayable, ClaimContract, Jsonable, JsonSerializ
      * @return array
      */
     #[\ReturnTypeWillChange]
-    public function jsonSerialize()
+    public function jsonSerialize(): mixed
     {
         return $this->toArray();
     }
